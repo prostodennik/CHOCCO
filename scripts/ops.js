3b714f0bc147552c7b87bdaed717d535f4e5fdd1
Денис Демин <denisdemin@MacBook-Air-Denis.local>
2021-04-07 22:56:00 +0300

    week 5 swipe fix

--- scripts/ops.js
+++ scripts/ops.js
@@ -1,129 +1,149 @@
 const section = $(".section");
 const display = $(".maincontent");
+const sideMenu = $(".fixed-menu__link");
+const fixedMenu = $(".fixed-menu");
+const menuItems = fixedMenu.find(".fixed-menu__item");
+
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
 
 let inScroll = false;
 
 section.first().addClass("active-scroll");
 
-const perfomTransition = sectionEq => {
+const countSectionPosition = sectionEq => {
+    const position = sectionEq * -100;
 
-    if (inScroll == false) {
-        inScroll = true;
-        const position = sectionEq * -100;
-        
-        const currentSection = section.eq(sectionEq);
-        const menuTheme = currentSection.attr("data-sidemenu-theme");
-        const sideMenu = $(".fixed-menu__link");
+    if (isNaN(position)) {
+        console.error("переданно не верное значение в countSectionPosition");
+        return 0;
 
-        if (menuTheme == "black") {
-            sideMenu.addClass("fixed-menu_black");
-        } else {
-            sideMenu.removeClass("fixed-menu_black");
-        }
+    }
 
-        display.css({
-            transform: `translateY(${position}%)`
-        });
-    
-        section.eq(sectionEq).addClass("active-scroll").siblings().removeClass("active-scroll");
-
-        const fixedMenu = $(".fixed-menu");
-
-        
-        setTimeout(() => {
-            inScroll = false;
-            
-            fixedMenu.find(".fixed-menu__item")
-            .eq(sectionEq)
-            .addClass("fixed-menu__item_active")
-            .siblings()
-            .removeClass("fixed-menu__item_active");
-         }, 1300);
-    };
+    return position
 }
 
-const scrollViewport = direction => {
+const changeMenuThemeForSection = sectionEq => {
+    const currentSection = section.eq(sectionEq);
+    const menuTheme = currentSection.attr("data-sidemenu-theme");
+    const activeClass = "fixed-menu_black";
+
+
+    if (menuTheme == "black") {
+        sideMenu.addClass(activeClass);
+    } else {
+        sideMenu.removeClass(activeClass);
+    }
+}
+
+const resetActiveClassForItem = (items, itemEq, activeClass) => {
+    items.eq(itemEq).addClass(activeClass).siblings().removeClass(activeClass);
+}
+
+const perfomTransition = sectionEq => {
+    if (inScroll) return;
+
+    const transitionOver = 1000;
+    const mouseInertiaOver = 300;
+
+    inScroll = true;
+
+    const position = countSectionPosition(sectionEq);
+
+    changeMenuThemeForSection(sectionEq);
+
+    display.css({
+        transform: `translateY(${position}%)`
+    });
+
+    resetActiveClassForItem(section, sectionEq, "active-scroll");
+
+
+    setTimeout(() => {
+        inScroll = false;
+        resetActiveClassForItem(menuItems, sectionEq, "fixed-menu__item_active");
+
+    }, transitionOver + mouseInertiaOver);
+}
+
+const viewportScroller = () => {
     const activeSection = section.filter(".active-scroll");
     const nextSection = activeSection.next();
     const prevSection = activeSection.prev();
 
-    if (direction == "next" && nextSection.length) {
-        perfomTransition(nextSection.index())
-    }
-
-    if (direction == "prev" && prevSection.length) {
-        perfomTransition(prevSection.index())
-    }
-}
+    return {
+        next() {
+            if (nextSection.length) {
+                perfomTransition(nextSection.index())
+            }
+        },
+        prev() {
+            if (prevSection.length) {
+                perfomTransition(prevSection.index())
+            }
+        },
+    };
+};
 
 $(window).on("wheel", e => {
     const deltaY = e.originalEvent.deltaY;
+    const scroller = viewportScroller();
 
     if (deltaY > 0) {
-        scrollViewport("next");
-    } 
+        scroller.next();
+    }
 
-    if (deltaY < 0 ) {
-        scrollViewport("prev");
+    if (deltaY < 0) {
+        scroller.prev();
     }
 });
 
-$(window).on("keydown", e => {
-    
+$(window).on("keydown", (e) => {
     const tagName = e.target.tagName.toLowerCase();
+    const userTypingInInput = tagName == "input" || tagName == "textarea";
+    const scroller = viewportScroller();
 
-    if (tagName !="input" && tagName != "textarea") {
-        switch (e.keyCode) {
-            case 38:
-                scrollViewport("prev");
-                break;
-    
-                case 40:
-                scrollViewport("next");     
-                break;
-        }
+    if (userTypingInInput) return;
+    switch (e.keyCode) {
+        case 38:
+            scroller.prev();
+            break;
+
+        case 40:
+            scroller.next();
+            break;
     }
+});
+
+$(".wrapper").on("touchmove", e => e.preventDefault());
+
+$("[data-scroll-to]").click(e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-scroll-to");
+    const reqSection = $(`[data-section-id=${target}]`);
+
+    perfomTransition(reqSection.index());
 
 });
 
-$(".wrapper").on("touchmove",e => e.preventDefault()); 
-
- $("[data-scroll-to]").click (e => {
-     e.preventDefault();
-
-     const $this = $(e.currentTarget);
-     const target = $this.attr("data-scroll-to");
-     const reqSection = $(`[data-section-id=${target}]`);
-
-     perfomTransition(reqSection.index());
-
- });
-
-// const mobileDetect = new MobileDetect(window.navigator.userAgent);
-// const isMobile = mobileDetect.mobile();
-
-
+if (isMobile) {
     //https://github.com/mattbryson/TouchSwipe-Jquery-Plugin 
-    // $("body").swipe({
-    //      swipe: function(event,direction) {
-    //        const scroller = viewportScroller();
-    //        let scrollDirection = "";
-   
-    //        if (direction == "up") scrollDirection = "next";
-    //        if (direction == "down") scrollDirection = "prev";
-           
-    //        scroller[scrollDirection](); 
-    //      },
-    //    });
+    $("body").swipe({
+        swipe: function (event, direction) {
+            const scroller = viewportScroller();
+            let scrollDirection = "";
+
+            if (direction == "up") scrollDirection = "next";
+            if (direction == "down") scrollDirection = "prev";
+
+            scroller[scrollDirection]();
+        },
+    });
+}
 
 
-    $(function() {
-        $("body").swipe( {
-          swipe:function(event, direction) {
-            $(this).text("You swiped " + direction );  
-          }
-        });
-      
-        $("body").swipe( {fingers:2} );
-      });
-       
+
+
+
